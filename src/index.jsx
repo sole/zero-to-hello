0870af5d130140374bdb36b2e2eb9425b58fb9c8
sole <listas@soledadpenades.com>
2023-09-12 15:19:02 +0100

    Use React to render something

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
+import { createRoot } from 'react-dom/client';
+
 document.addEventListener('DOMContentLoaded', () => {
 	let appElement = document.getElementById('app');
-	appElement.innerHTML = '<h1>hello from my app</h1>';
+	let appRoot = createRoot(appElement);
+
+	appRoot.render(<h1>hello via React!</h1>);
+	// Roughly equivalent to the above
+	// appElement.innerHTML = '<h1>hello from my app</h1>';
 });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import react from '@vitejs/plugin-react';
+
 export default {
 	build: {
 		outDir: '../build',
 		emptyOutDir: true
 	},
+	plugins: [react()],
 	root: './src'
 }
