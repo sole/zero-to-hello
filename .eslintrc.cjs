5b680bc32770c479962c928471affa6d4e3aaf7f
sole <listas@soledadpenades.com>
2023-09-12 16:36:06 +0100

    Lint JSX too

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,5 +2,10 @@ module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
 	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-	ignorePatterns: ['build', '.eslintrc.cjs']
+	ignorePatterns: ['build', '.eslintrc.cjs'],
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime'
+	],
+	settings: { react: { version: '18.2' } },
 }
