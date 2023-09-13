5a7d74a3fdd143f457c3c2246f9680ca20c61842
sole <listas@soledadpenades.com>
2023-09-13 13:47:19 +0100

    add other recommended ESLint settings and plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,16 @@ module.exports = {
 	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
 	ignorePatterns: ['build', '.eslintrc.cjs'],
 	extends: [
+		'eslint:recommended',
 		'plugin:react/recommended',
 		'plugin:react/jsx-runtime'
 	],
 	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+	  'react-refresh/only-export-components': [
+		'warn',
+		{ allowConstantExport: true },
+	  ],
+	},
 }
