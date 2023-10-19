024428ac9ba224ab4ccf5f87b500247791f55c99
sole <listas@soledadpenades.com>
2023-10-19 14:31:03 +0100

    Use ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,34 @@
 import globals from "globals";
 
-export default [{
-	files: ["src/**/*.js", "src/**/*.jsx"],
-	languageOptions: {
-		globals: {
-			...globals.browser
+export default [
+	{
+		files: ["src/**/*.js", "src/**/*.jsx"],
+		languageOptions: {
+			globals: {
+				...globals.browser
+			},
+			parserOptions: {
+				ecmaVersion: 2020
+			}
 		},
-		parserOptions: {
-			ecmaVersion: 2020
-		}
+		extends: [
+			'eslint:recommended',
+			'plugin:react/recommended',
+			'plugin:react/jsx-runtime'
+		],
+		settings: { react: { version: '18.2' } },
+		plugins: ['react-refresh'],
+		rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		},
 	},
-	ignorePatterns: ['build', '.eslintrc.cjs'],
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:react/jsx-runtime'
-	],
-	settings: { react: { version: '18.2' } },
-	plugins: ['react-refresh'],
-	rules: {
-	  'react-refresh/only-export-components': [
-		'warn',
-		{ allowConstantExport: true },
-	  ],
-	},
-}];
+	{
+		ignores: [
+			"build/**/*.js",
+			"**/**/*.config.js"
+		]
+	}
+];
